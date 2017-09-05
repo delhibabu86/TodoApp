3ae8f1c0386d05b3ec2f77f7534ae29314b02e06
Arnab Karmakar <arnab@binary.com>
2017-09-05 14:47:46 -0600

    Add dummy tests

---
+++ src/actions/__tests__/other.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Arnab Karmakar on 9/5/17.
+ */
+//import { changeText, CHANGE_TEXT } from '../other';
+//import {expect} from 'chai';
+
+describe('Test other.js', () => {
+  const text = 'test';
+  const expected = {
+    //type: CHANGE_TEXT,
+    //text: text,
+  };
+  //expect(changeText(text)).to.be.equal(expected);
+});
